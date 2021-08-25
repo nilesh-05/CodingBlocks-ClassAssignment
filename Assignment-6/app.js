749589542a153420e7179e679b64841afe714da3
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-08-25 16:43:31 +0530

    remove element bug

--- Assignment-6/app.js
+++ Assignment-6/app.js
@@ -17,18 +17,9 @@ for (i = 0; i < close.length; i++) {
 	};
 }
 
-var list = document.querySelector("ul");
-list.addEventListener(
-	"click",
-	function (ev) {
-		if (ev.target.tagName === "LI") {
-			ev.target.classList.toggle("checked");
-		}
-	},
-	false,
-);
 const addItem = () => {
 	var li = document.createElement("li");
+	var ul = document.querySelector("ul");
 	var inputValue = document.getElementById("myInput").value;
 	var t = document.createTextNode(inputValue);
 	li.appendChild(t);
@@ -43,8 +34,13 @@ const addItem = () => {
 	var txt = document.createTextNode("\u00D7");
 	span.className = "close";
 	span.appendChild(txt);
+	li.className = "draggable";
+	var attr = document.createAttribute("draggable");
+	attr.value = "true";
+	li.setAttributeNode(attr);
 	li.appendChild(span);
-
+	// ul.appendChild(li);
+	addEventsDragAndDrop(li);
 	for (i = 0; i < close.length; i++) {
 		close[i].onclick = function () {
 			var div = this.parentElement;
@@ -52,3 +48,55 @@ const addItem = () => {
 		};
 	}
 };
+
+// Dragging
+function dragStart(e) {
+	this.style.opacity = "0.4";
+	dragSrcEl = this;
+	e.dataTransfer.effectAllowed = "move";
+	e.dataTransfer.setData("text/html", this.innerHTML);
+}
+
+function dragEnter(e) {
+	this.classList.add("over");
+}
+
+function dragLeave(e) {
+	e.stopPropagation();
+	this.classList.remove("over");
+}
+
+function dragOver(e) {
+	e.preventDefault();
+	e.dataTransfer.dropEffect = "move";
+	return false;
+}
+
+function dragDrop(e) {
+	if (dragSrcEl != this) {
+		dragSrcEl.innerHTML = this.innerHTML;
+		this.innerHTML = e.dataTransfer.getData("text/html");
+	}
+	return false;
+}
+
+function dragEnd(e) {
+	var listItens = document.querySelectorAll(".draggable");
+	[].forEach.call(listItens, function (item) {
+		item.classList.remove("over");
+	});
+	this.style.opacity = "1";
+}
+
+function addEventsDragAndDrop(el) {
+	el.addEventListener("dragstart", dragStart, false);
+	el.addEventListener("dragenter", dragEnter, false);
+	el.addEventListener("dragover", dragOver, false);
+	el.addEventListener("dragleave", dragLeave, false);
+	el.addEventListener("drop", dragDrop, false);
+	el.addEventListener("dragend", dragEnd, false);
+}
+var listItens = document.querySelectorAll(".draggable");
+[].forEach.call(listItens, function (item) {
+	addEventsDragAndDrop(item);
+});
