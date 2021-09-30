90fd6014e5f9740c93d0a2759944f971ef48347b
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-09-30 21:01:12 +0530

    homepage done

--- Assignment-8/index.js
+++ Assignment-8/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	}),
+);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -14,6 +20,19 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
 	res.render("about");
+});
+
+app.get("/formpage", (req, res) => {
+	res.render("formpage");
+});
+
+app.post("/formpage", (req, res) => {
+	var email = req.body.mail;
+	var phoneNumber = Number(req.body.phone);
+	var name = req.body.name;
+	var checkout = req.body.checkout == undefined ? "no" : "yes";
+	console.log(email, phoneNumber, name, checkout);
+	res.render("index");
 });
 
 app.listen(PORT, () => console.log(`server up and running at ${PORT}`));
